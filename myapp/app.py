ee07e4e09cc952d4a7d6c88d3eca535db5b1fb12
Erdi Myftaraga <erdi.myftaraga@ngi.no>
2023-03-16 14:09:54 +0100

    Commit new app.

--- myapp/app.py
+++ myapp/app.py
@@ -1,33 +1,56 @@
+# This app is translated from Mastering Shinywidgets
+# https://mastering-shiny.org/basic-reactivity.html#reactive-expressions-1
 from shiny import App, render, ui
-import numpy as np
-import matplotlib.pyplot as plt
+from numpy import random
+
+# Functions we import from stats.py
+from stats import freqpoly, t_test
 
 app_ui = ui.page_fluid(
-    ui.layout_sidebar(
-        ui.panel_sidebar(
-            ui.input_slider("period", "Period", 0.5, 4, 1, step=0.5),
-            ui.input_slider("amplitude", "Amplitude", 0, 2, 1, step=0.25),
-            ui.input_slider("shift", "Phase shift", 0, 2, 0, step=0.1),
+    ui.row(
+        ui.column(
+            4,
+            "Distribution 1",
+            ui.input_numeric("n1", label="n", value=1000, min=1),
+            ui.input_numeric("mean1", label="µ", value=0, step=0.1),
+            ui.input_numeric("sd1", label="σ", value=0.5, min=0.1, step=0.1),
         ),
-        ui.panel_main(
-            ui.output_plot("plot"),
+        ui.column(
+            4,
+            "Distribution 2",
+            ui.input_numeric("n2", label="n", value=1000, min=1),
+            ui.input_numeric("mean2", label="µ", value=0, step=0.1),
+            ui.input_numeric("sd2", label="σ", value=0.5, min=0.1, step=0.1),
         ),
+        ui.column(
+            4,
+            "Frequency polygon",
+            ui.input_numeric("binwidth", label="Bin width", value=0.1, step=0.1),
+            ui.input_slider("range", label="range", value=[-3, 3], min=-5, max=5),
+        ),
+    ),
+    ui.row(
+        ui.column(9, ui.output_plot("hist")),
+        ui.column(3, ui.output_text_verbatim("ttest")),
     ),
 )
 
 
 def server(input, output, session):
     @output
-    @render.plot(alt="Sine wave")
-    def plot():
-        t = np.arange(0.0, 4.0, 0.01)
-        s = input.amplitude() * np.sin(
-            2 * np.pi / input.period() * (t - input.shift() / 2)
-        )
-        fig, ax = plt.subplots()
-        ax.set_ylim([-2, 2])
-        ax.plot(t, s)
-        ax.grid()
+    @render.plot
+    def hist():
+        print(input.range())
+        x1 = random.normal(input.mean1(), input.sd1(), input.n1())
+        x2 = random.normal(input.mean2(), input.sd2(), input.n2())
+        return freqpoly(x1, x2, input.binwidth(), input.range())
+
+    @output
+    @render.text
+    def ttest():
+        x1 = random.normal(0, 1, 100)
+        x2 = random.normal(0, 1, 100)
+        return t_test(x1, x2)
 
 
 app = App(app_ui, server)
